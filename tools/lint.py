8740436f4e7eae98ed1fdb87488c9d987416123c
Benedict Harcourt <ben.harcourt@harcourtprogramming.co.uk>
2022-04-12 21:27:03 +0000

    further work

--- tools/lint.py
+++ tools/lint.py
@@ -2,80 +2,166 @@
 
 from __future__ import annotations
 
-import difflib  # heelo
+from typing import Generator, Literal, Set
+
+import dataclasses
+import os
 import subprocess
+import sys
 
 
-def lint_black(*paths: str) -> bool:
-    args = ["black", "--diff", "--no-color", "--quiet"]
-    args.extend(paths)
+@dataclasses.dataclass
+class Annotation:
+    """Schema for a github action annotation, representing an error"""
 
-    result = subprocess.run(args, stdin=subprocess.DEVNULL, capture_output=True, check=False)
+    level: Literal["error", "warning", "notice"]
+    file: str
+    line: int
+    col: int
+    title: str
+    message: str
 
-    errors = result.stderr.decode("utf-8").split("\n")
+    def __str__(self) -> str:
+        mess = self.message.replace("\n", "%0A")
+        return f"::{self.level} file={self.file},line={self.line},col={self.col},title={self.title}::{mess}"
 
-    for error in errors:
-        error = error.strip()
+    def __lt__(self, other):
+        if not (isinstance(other, Annotation)):
+            return False
 
-        if not error:
-            continue
-
-        level, header, message, line, char, info = error.split(":", 5)
-        header, _, file = header.rpartition(" ")
-
-        level = level if level in ["notice", "warning", "error"] else "error"
-
-        print(f"::{level} file={file},line={line},col={char},title={message.strip()}::{info.strip()}")
-
-    file = ""
-    line = 0
-    buffer = ""
-
-    for diffline in result.stdout.decode("utf-8").split("\n"):
-        if diffline.startswith("+++ "):
-            continue
-
-        if diffline.startswith("--- "):
-            if file and buffer:
-                buffer = buffer.replace('\n', '%0A')
-                print(f"::error file={file},line={line},col=1::{buffer}")
-            buffer = ""
-            file, _ = diffline[4:].split("\t")
-            continue
-
-        if diffline.startswith("@@"):
-            if file and buffer:
-                buffer = buffer.replace('\n', '%0A')
-                print(f"::error file={file},line={line},col=1::{buffer}")
-            buffer = ""
-
-            _, start, _, _ = diffline.split(" ")
-            line, _ = start.split(",")
-            line = abs(int(line))
-            continue
-
-        buffer += diffline + "\n"
-
-    return False  # For now, just assume errors occurred
+        return self.file < other.file or self.file == other.file and self.line < other.line
 
 
-def lint_pylint(*paths: str):
-    args = ["pylint"]
-    args.extend(paths)
+class LintToolchain:
+    """Wrapper class for running linting tools, and outputting GitHub annotations"""
 
-    result = subprocess.run(args, stdin=subprocess.DEVNULL, capture_output=True, check=False)
+    folders = Set[str]
+    is_ci: bool
+    success: bool
 
-    for line in result.stdout.decode("utf-8").split("\n"):
-        if ":" not in line:
-            continue
+    def __init__(self, *folders: str, ci: bool) -> None:
+        self.folders = set(folders)
+        self.is_ci = ci
+        self.success = True
 
-        try:
-            file, line_no, col, error = line.strip().split(":", 3)
-            print(f"::error file={file},line={line_no},col={col}::{error}")
-        except ValueError:
-            pass
+    def __call__(self) -> Generator[Annotation, None, None]:
+        yield from self.lint_black()
+        yield from self.lint_flake8()
+        yield from self.lint_pylint()
+
+    def run_tool(self, name: str, *args: str) -> subprocess.CompletedProcess:
+        arg_list = list(args)
+        arg_list.extend(self.folders)
+
+        run = subprocess.run(
+            arg_list, stdin=subprocess.DEVNULL, capture_output=self.is_ci, check=False
+        )
+
+        if run.returncode:
+            self.success = False
+
+        if self.is_ci:
+            print(f"::group::{name}")
+            sys.stdout.write(run.stdout.decode("utf-8"))
+            sys.stdout.write(run.stderr.decode("utf-8"))
+            print(f"::endgroup::")
+        else:
+            run.stdout = b""
+            run.stderr = b""
+
+        return run
+
+    def lint_black(self) -> Generator[Annotation, None, None]:
+        args = ["black"]
+
+        if self.is_ci:
+            args.extend(["--diff", "--no-color", "--quiet"])
+
+        result = self.run_tool("Black (Formatter)", *args)
+
+        yield from self.lint_black_errors(result)
+        yield from self.lint_black_diffs(result)
+
+    def lint_black_errors(self, result):
+        errors = result.stderr.decode("utf-8").split("\n")
+        for error in errors:
+            error = error.strip()
+
+            if not error:
+                continue
+
+            level, header, message, line, char, info = error.split(":", 5)
+            header, _, file = header.rpartition(" ")
+
+            level = level if level in ["notice", "warning", "error"] else "error"
+
+            yield Annotation(level, file, int(line), int(char), message.strip(), info.strip())
+
+    def lint_black_diffs(self, result):
+        file = ""
+        line = 0
+        buffer = ""
+        for diff_line in result.stdout.decode("utf-8").split("\n"):
+            if diff_line.startswith("+++ "):
+                continue
+
+            if diff_line.startswith("--- "):
+                if file and buffer:
+                    yield Annotation("error", file, line, 1, "Black alteration", buffer)
+
+                buffer = ""
+                file, _ = diff_line[4:].split("\t")
+                continue
+
+            if diff_line.startswith("@@"):
+                if file and buffer:
+                    yield Annotation("error", file, line, 1, "Black alteration", buffer)
+
+                _, start, _, _ = diff_line.split(" ")
+                _line, _ = start.split(",")
+                line = abs(int(_line))
+                buffer = ""
+                continue
+
+            buffer += diff_line + "\n"
+
+    def lint_flake8(self) -> Generator[Annotation, None, None]:
+        result = self.run_tool("Flake8", "flake8")
+
+        for line in result.stdout.decode("utf-8").split("\n"):
+            if ":" not in line:
+                continue
+
+            try:
+                file, line_no, col, error = line.strip().split(":", 3)
+                yield Annotation("error", file, int(line_no), int(col), "", error.strip())
+            except ValueError:
+                pass
+
+    def lint_pylint(self) -> Generator[Annotation, None, None]:
+        result = self.run_tool("PyLint", "pylint")
+
+        for line in result.stdout.decode("utf-8").split("\n"):
+            if ":" not in line:
+                continue
+
+            try:
+                file, line_no, col, error = line.strip().split(":", 3)
+                yield Annotation("error", file, int(line_no), int(col), "", error)
+            except ValueError:
+                pass
 
 
-if __name__ == '__main__':
-    lint_black('src', 'examples', 'tools')
-    lint_pylint('src', 'examples', 'tools')
+if __name__ == "__main__":
+    is_ci = "GITHUB_ACTION" in os.environ
+
+    linter = LintToolchain("src", "examples", "tools", ci=is_ci)
+    issues = list(linter())
+
+    if is_ci:
+        print("::group::Annotations")
+        print(sorted(issues))
+        print("::endgroup::")
+        print("Total Issues:", len(issues))
+
+    sys.exit(not linter.success or len(issues) > 0)
